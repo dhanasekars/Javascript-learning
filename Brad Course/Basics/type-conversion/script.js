cab8feb76d577f5d77d9331f7093b967863cb9fc
DS <dhanasekars@gmail.com>
2023-07-06 09:01:26 +0530

    daily checkin

---
+++ Brad Course/Basics/type-conversion/script.js
@@ -0,0 +1,32 @@
+// let myString = "Developer";
+
+// let myNewString = myString[0].toUpperCase() + myString.slice(1).toLowerCase();
+
+// console.log(myNewString);
+// console.log(Math) 
+
+// let x = Math.floor(Math.random()*100 + 1);
+// let y = Math.floor(Math.random()*50 + 1);
+
+// sumOutput = `${x} + ${y} = ${x + y}`
+// differenceOutput = `${x} - ${y} = ${x - y}`
+// productOutput = `${x} * ${y} = ${x * y}`
+// quotientOutput = `${x} / ${y} = ${x / y}`
+// rmOutput = `${x} % ${y} = ${x % y}` 
+
+// // console.log(Math)
+// console.log(x, y)
+// console.log(sumOutput)
+// console.log(differenceOutput)   
+// console.log(productOutput)
+// console.log(quotientOutput)
+// console.log(rmOutput)
+
+
+let d = new Date() 
+console.log(d)
+console.log(Date.now())
+console.log(Date.now())
+
+console.log(d.getFullYear())
+
