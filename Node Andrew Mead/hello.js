0b6f8f79a7ad7a7e08c5b2ff56c94c8d3057d00c
DS <dhanasekars@gmail.com>
2023-08-25 12:26:54 +0530

    gitignore update

--- Node Andrew Mead/hello.js
+++ Node Andrew Mead/hello.js
@@ -1,1 +1,23 @@
-console.log("Welcome to the class!")
+//  Arrow function challenge
+
+let tasks = {
+    tasks: [
+        {
+            text: "Grocery shopping",
+            completed: true,
+        },
+        {
+            text: "Clean yard",
+            completed: false,
+        },
+        {
+            text: "Film course",
+            completed: false,
+        },
+    ],
+    getTasksToDo() {
+        return this.tasks.filter((task) => task.completed === false)
+    },
+}
+
+console.log(tasks.getTasksToDo())
